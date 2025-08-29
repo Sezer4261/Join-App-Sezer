1d7dc554899442b61083245372603b05ad585f8e
Surab Link <surab.l@hotmail.de>
2025-08-29 22:55:35 +0200

    sidebar highlight

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,66 +1,26 @@
 function sidebarHighlightingSummary() {
     window.location.href = "summary.html";
-
-    document.getElementById("summary-li").classList = "highlighted";
-    document.getElementById("add-task-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("board-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("contacts-li").classList = "sidebar-li-hover-effect";
-
-    document.getElementById("privacy-policy-div").classList = "sidebar-footer-link-hover-effect";
-    document.getElementById("legal-notice-div").classList = "sidebar-footer-link-hover-effect";
 }
 
 function sidebarHighlightingAddTask() {
     window.location.href = "addTask.html";
-
-    document.getElementById("summary-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("add-task-li").classList = "highlighted";
-    document.getElementById("board-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("contacts-li").classList = "sidebar-li-hover-effect";
-
-    document.getElementById("privacy-policy-div").classList = "sidebar-footer-link-hover-effect";
-    document.getElementById("legal-notice-div").classList = "sidebar-footer-link-hover-effect";
 }
 
 function sidebarHighlightingBoard() {
     window.location.href = "board.html";
-
-    document.getElementById("summary-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("add-task-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("board-li").classList = "highlighted";
-    document.getElementById("contacts-li").classList = "sidebar-li-hover-effect";
-
-    document.getElementById("privacy-policy-div").classList = "sidebar-footer-link-hover-effect";
-    document.getElementById("legal-notice-div").classList = "sidebar-footer-link-hover-effect";
 }
 
 function sidebarHighlightingContacts() {
-    document.getElementById("summary-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("add-task-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("board-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("contacts-li").classList = "highlighted";
+    window.location.href = "contacts.html";
 
-    document.getElementById("privacy-policy-div").classList = "sidebar-footer-link-hover-effect";
-    document.getElementById("legal-notice-div").classList = "sidebar-footer-link-hover-effect";
 }
 
 function sidebarHighlightingPrivacyPolicy() {
-    document.getElementById("summary-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("add-task-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("board-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("contacts-li").classList = "sidebar-li-hover-effect";
+    window.location.href = "privacy-policy.html";
 
-    document.getElementById("privacy-policy-div").classList = "highlighted";
-    document.getElementById("legal-notice-div").classList = "sidebar-footer-link-hover-effect";
 }
 
 function sidebarHighlightingLegalNotice() {
-    document.getElementById("summary-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("add-task-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("board-li").classList = "sidebar-li-hover-effect";
-    document.getElementById("contacts-li").classList = "sidebar-li-hover-effect";
-
-    document.getElementById("privacy-policy-div").classList = "sidebar-footer-link-hover-effect";
-    document.getElementById("legal-notice-div").classList = "highlighted";
+    window.location.href = "legal-notice.html";
 }
 
